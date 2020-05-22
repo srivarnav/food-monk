30eb265ce0ec0fab5c31cdba6b84bad63d1cb5f8
Arnav Srivastava <srivarnav@gmail.com>
2020-05-23 01:22:03 +0530

    Updates

---
+++ precache-manifest.8a2dd58fe72ccfcc942fa9f1e72baad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ed5ac74a333fe865163d0671e0f7e8",
+    "url": "/food-monk/index.html"
+  },
+  {
+    "revision": "dce96844487760cde4ba",
+    "url": "/food-monk/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e92493fdcc962d8c10cc",
+    "url": "/food-monk/static/js/2.2cb0c32e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/food-monk/static/js/2.2cb0c32e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce96844487760cde4ba",
+    "url": "/food-monk/static/js/main.c296909e.chunk.js"
+  },
+  {
+    "revision": "b8c0bd5755c3eddeb108",
+    "url": "/food-monk/static/js/runtime-main.49607931.js"
+  }
+]);
